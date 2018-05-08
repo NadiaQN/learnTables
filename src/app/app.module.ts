70dc43edeb376d49fcd13508431e923c8d817302
RCD-YY <quezada.nadia@gmail.com>
2018-05-08 18:54:20 -0300

    inicializando repo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTableModule, MatSortModule } from '@angular/material'; 
+ 
 import { AppComponent } from './app.component';
+import { UsertableComponent } from './usertable/usertable.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsertableComponent
   ],
   imports: [
     BrowserModule
